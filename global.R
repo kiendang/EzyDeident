7886b50d15fff9cdf1fd7bf802e1c45d1d23383b
kiendang <kiend@pm.me>
2018-11-21 23:16:17 +0800

    Add time shift tool

--- global.R
+++ global.R
@@ -9,3 +9,12 @@ library(digest)
 
 source('masking_code.R')
 source('functions.R')
+
+read_sheet <- function(path) {
+  switch(
+    tools::file_ext(path),
+    xls = read_xls(path, guess_max = 10000),
+    xlsx = read_xlsx(path, guess_max = 10000),
+    data.table::fread(path, data.table = FALSE)
+  ) %>% as.data.frame
+}
--- ui.R
+++ ui.R
@@ -197,6 +197,94 @@ shinyUI(
       )
     ),
     
+    navbarMenu(
+      "Time Shift",
+      tabPanel(
+        "Assign shifting period",
+        fluidRow(
+          column(
+            width = 4,
+            h5("Upload original data"),
+            fileInput(
+              "original-file", NULL,
+              accept = c(
+                "application/vnd.ms-excel",
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "text/csv",
+                "text/plain",
+                ".csv", ".tsv", ".txt"
+              )
+            )
+          ),
+          column(
+            width = 4,
+            h5("Select identifier column"),
+            uiOutput("identifier-ui")
+          ),
+          column(
+            width = 4,
+            h5("Specify shifting range (years)"),
+            numericInput("from", NULL, 100L, NA, 1L),
+            numericInput("to", NULL, 300L, NA, 1L)
+          )
+        ),
+        fluidRow(
+          column(
+            width = 4,
+            h5("Download mappings"),
+            downloadButton("download-mappings", "Download",
+                           class = "btn-primary")
+          )
+        )
+      ),
+      
+      tabPanel(
+        "Apply shifting",
+        fluidRow(
+          column(
+            width = 4,
+            h5("Upload original data"),
+            fileInput(
+              "unshifted-file",
+              NULL,
+              accept = c(
+                "application/vnd.ms-excel",
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+              )
+            )
+          ),
+          column(
+            width = 4,
+            h5("Select identifier column"),
+            uiOutput("unshifted-identifier-ui")
+          ),
+          column(
+            width = 4,
+            h5("Upload mappings"),
+            fileInput(
+              "mappings-upload",
+              NULL,
+              accept = c(
+                "application/vnd.ms-excel",
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "text/csv",
+                "text/plain",
+                ".csv", ".tsv", ".txt"
+              )
+            )
+          )
+        ),
+        fluidRow(
+          column(
+            width = 4,
+            h5("Download shifted data"),
+            downloadButton("download-shifted", "Download",
+                           class = "btn-primary")
+          )
+        )
+      )
+    ),
+    
     tabPanel(
       "About",
       fluidRow(
